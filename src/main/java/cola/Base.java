6d31c771b7cd5d90a23c56ea45489bb184b6b8c5
kaki309 <andres_gab.fernandez@uao.edu.co>
2024-02-14 19:54:57 -0500

    Initial commit

---
+++ src/main/java/cola/Base.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+/**
+ *
+ * @author andres_gab.fernandez
+ */
+public abstract class Base {
+    
+    public abstract Base copy(); 
+    
+}
---
+++ src/main/java/cola/Cola.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author andres_gab.fernandez
+ */
+public class Cola <T extends Base>{
+    
+    private LinkedList<T> elementos;
+    
+    public Cola(){
+        elementos = new LinkedList<>();
+    }
+    
+    public void encolar(T elemento){
+        elementos.addLast(elemento);
+    }
+    
+    public T desencolar(){
+        return elementos.removeFirst();
+    }
+    
+    public boolean estaVacia(){
+        return elementos.isEmpty();
+    }
+    
+    
+}
